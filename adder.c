26f5dae56e4171aa85f6fde2ff14b077f89ec4dc
juyohan <dygks8556@naver.com>
2016-07-22 16:27:19 +0900

    adder

--- adder.c
+++ adder.c
@@ -9,6 +9,7 @@ int add(int n[], int count);
 int main(void) {
   int i = 0, a;
   int sum, g[MAX];
+  float avg;
 
   srand(time(NULL));
   printf("1부터 10사이의 양수 하나를 고르시오. : ");
@@ -23,12 +24,14 @@ int main(void) {
     g[i] = rand() % 10;
 
   sum = add(g, a);
+  avg = (float) sum / a;
 
   for (i = 0; i < a; i++) {
     printf("%d\n", g[i]);
   }
 
   printf("전체 합은 %d 입니다.\n", sum);
+  printf("평균은 %.2lf 입니다.\n",avg);
 
   return 0;
 }
