170609e016c1d9407973d22c855ac62499e454e9
juyohan <dygks8556@naver.com>
2016-09-09 09:45:17 +0900

    srand 추가

--- study7.c
+++ study7.c
@@ -7,10 +7,12 @@
 int main(void)
 {
     int index = 0;
+
+    srand(time(NULL));
     printf("이 예제는 rand()함수를 이용하여 5개의 난수를 생성하는 예제입니다.\n");
 
     for (index=0;index<MAX;index++)
-    printf("%d",rand());
+    printf("%4d",rand());
     printf("\n");
 
     return 0;
