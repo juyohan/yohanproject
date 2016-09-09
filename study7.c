ea8f30e250888ac5cfe51584c91f3f1807406932
juyohan <dygks8556@naver.com>
2016-09-09 14:27:32 +0900

    로또 프로그램

--- study7.c
+++ study7.c
@@ -12,7 +12,6 @@ int main(void)
 {
     int leg;
     char inp[5];
-    srand(time(NULL));
     printf("로또 프로그램을 실행시키시겠습니까?\n");
     printf("실행하실려면 'yes'를 안하시려면 'no'를 입력해주세요. :");
     scanf("%s",inp);
@@ -22,8 +21,7 @@ int main(void)
         lotto();
     }
     else if (leg == 2){
-        printf("로또 프로그램을 종료합니다.");
-        break;
+        printf("로또 프로그램을 종료합니다.\n");
     }
     return 0;
 }
@@ -31,6 +29,7 @@ int main(void)
 int lotto(num)
 {
     int index=0;
+    srand(time(NULL));
 
     for (index=0;index<MAX;index++){
         printf("%d",1 + (rand() % 45));
