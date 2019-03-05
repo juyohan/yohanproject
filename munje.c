6dfceae53f1581725913019c1a2f3c981d8bb2be
juyohan <dygks8556@naver.com>
2019-03-05 09:17:18 +0900

    연습문제

---
+++ munje.c
@@ -0,0 +1,48 @@
+// #include <stdio.h>
+
+// int main(){
+//     printf("Fun Programming!");   
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(void){
+//     printf("(@) (@)\n");
+//     printf("(=^.^=)\n");
+//     printf("(-m-m-)");
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     char *name;
+//     printf("이름을 적어주세요. : ");
+//     scanf("%s",name);
+//     int a,b;
+//     printf("키랑 몸무게 순서대로 적어주세요. : ");
+//     scanf("%d %d",&a,&b);
+    
+//     printf("%s의 키는 %d입니다.\n",name,a);
+//     printf("%s의 몸무게는 %d 입니다.",name,b);
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     int a;
+//     printf("구구단 몇 단을 출력하시겠습니까? : ");
+//     scanf("%d",&a);
+//     for (int i=1;i<11;i++){
+//         printf("%d * %d = %d\n",a,i,a*i); 
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    int 
+}
