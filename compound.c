4e87138e2a5fadd715508c9a39ed5ce661753396
juyohan <dygks8556@naver.com>
2019-03-27 10:41:16 +0900

    복리 계산을 이용

--- compound.c
+++ compound.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
+#include <math.h>
 
-double compound(int a,int b,double c);
-double increase(double m,int b);
+// double compound(int a,int b,double c);
+// double increase(double m,int b);
 
 int main(){
-    int money,year;
+    int money,total;
     double rate;
+    int year=1;
 
     printf("1. 원금을 입력해주세요. : ");
     scanf("%d",&money);
-    printf("2. 년수를 입력해주세요. : ");
-    scanf("%d",&year);
-    printf("3. 이율을 입력해주세요. : ");
+    printf("2. 이율을 입력해주세요. : ");
     scanf("%lf",&rate);
 
-    printf("총 값은 : %lf",compound(money,year,rate));
-}
-
-double compound(int a,int b,double c){
-    return a*increase(1+(0.01*c),b);
-}
-
-double increase(double m, int b){
-    int i;
-    double total = 1;
-
-    for (i = 0; i < b;i++){
-        total *= m;
+while (1) {
+    total = money * pow(1+(0.01*rate),year); // 이율의 제곱승을 해줌
+    year++; //년도의 값을 증가시킴
+    if (total >= 2*money){ // 총 금액이 원금의 2배보다 높거나 같으면 멈춤
+       break;
     }
-    return total;
 }
+
+    printf("%d년 후 원금의 2배가 됩니다. \n",year);
+
+    return 0;
+}
+
+// double compound(int a,int b,double c){ 
+//     return a*increase(1+(0.01*c),b); // 총 금액의 값
+// }
+
+// double increase(double m, int b){
+//     int i;
+//     double total = 1;
+
+//     for (i = 0; i < b;i++){
+//         total *= m;
+//     }
+//     return total;
+// }
