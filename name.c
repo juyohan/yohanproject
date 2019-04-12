9557272d9c7b2008a140586eec7ea75b2b6936a9
juyohan <dygks8556@naver.com>
2019-04-12 10:33:53 +0900

    구조체를 이용해 정보 입력, 출력

--- name.c
+++ name.c
@@ -1,26 +1,58 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 // 구조체를 이용해 이름,학번,학과출력
 
-// struct Introduction {
-//     char name[10], major[15];
-//     int num;
-// } i;
+struct Introduction {
+    char name[10][50];
+    char address[10][50];
+    char phone[10][50];
+    char age[10][50];
+} i;
 
-// int main(){
-//     struct Introduction i;
+int main(){
+    struct Introduction i;
 
-//     printf("이름을 입력해주세요. : ");
-//     scanf("%s",i.name);
-//     printf("학과를 적어주세요. : ");
-//     scanf("%s",i.major);
-//     printf("학번을 적어주세요. : ");
-//     scanf("%d",&i.num);
+    int num,a,num_;
 
-//     printf("이름 : %s\n학과 : %s\n학번 : %d\n",i.name,i.major,i.num);
+    printf("몇 명의 정보를 입력하시겠습니까?(최대 10명) : ");
+    scanf("%d",&num);
 
-//     return 0;
-// }
+    for (int j = 0 ; j<num; j++){
+        printf("%d번째 사람의 정보를 입력합니다.\n",j+1);
+    
+    printf("이름을 입력해주세요. : ");
+    scanf("%s",i.name[j]);
+    printf("주소를 적어주세요. : ");
+    scanf("%s",i.address[j]);
+    printf("나이을 적어주세요. : ");
+    scanf("%s",i.age[j]);
+    printf("핸드폰 번호를 적어주세요.(-포함) : ");
+    scanf("%s",i.phone[j]);
+
+    }
+    
+    while(1){
+    printf("정보를 확인하겠습니까?\n1.예 2.아니요 : ");
+    scanf("%d",&a);
+
+    if (a==1){
+        printf("몇 번째의 정보를 확인하겠습니까? : ");
+        scanf("%d",&num_);
+        printf("이름 : %s\n주소 : %s\n나이 : %s\n핸드폰 번호 : %s\n",i.name[num_-1],i.address[num_-1],i.age[num_-1],i.phone[num_-1]);
+    }
+        else if (a==2){
+            printf("입력하신 정보를 삭제하고 종료합니다.\n");
+            exit(1);
+        }
+        else {
+        printf("숫자를 잘못입력하셨습니다.\n이전으로 돌아갑니다.\n");
+        return 0;
+        }
+    }
+    return 0;
+}
 
 // 배열 초기화를 사용해 출력
 
@@ -41,13 +73,15 @@
 //     return 0;
 // }
 
-int main(){
-    char *name = "주요한";
-    char *address = "경기도 화성시 동탄순환대로 21길 15, 1342동 2502호";
-    char *major = "IT학부";
+// 포인터 배열을 사용해 출력 
 
-    printf("이름 : %s\n주소 : %s\n학과 : %s\n",name,address,major);
+// int main(){
+//     char *name = "주요한";
+//     char *address = "경기도 화성시 동탄순환대로 21길 15, 1342동 2502호";
+//     char *major = "IT학부";
 
-    return 0;
-}
+//     printf("이름 : %s\n주소 : %s\n학과 : %s\n",name,address,major);
+
+//     return 0;
+// }
 
