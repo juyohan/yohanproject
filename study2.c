9fd445999458914c536efcc8740439a4e6931e98
juyohan <dygks8556@naver.com>
2019-09-19 09:38:05 +0900

    1부터 100까지의 합

--- study2.c
+++ study2.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+
 int main()
 {
-    int i;
-    int sum=0;
+    int i , k =100;
+    int sum=0,sum1= 0 ,sum2=0;
+
     for (i=1;i<101;i++)
     {
         sum = sum + i;
        }
+    for (int j = 0; j < 50 ; j++){
+        sum1 = (k-j) + j;
+        sum2 += sum1;
+        
+    }
+    sum2 = sum2 + 50; 
+    
     printf("1부터 100까지의 합 : %d\n",sum);
+     printf("1부터 100까지의 합 : %d\n",sum2);
     return 0;
 }
